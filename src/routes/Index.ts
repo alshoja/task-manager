3b1e9d6a0c8e6d112cfe6158b7dfeb97ab412c9d
alshoja <alshoja@gmail.com>
2024-12-08 18:53:09 +0100

    fix: middleware in routes

--- src/routes/Index.ts
+++ src/routes/Index.ts
@@ -3,12 +3,13 @@ import taskRoutes from "./Task.route";
 import tagRoutes from "./Tag.route";
 import taskTagRoutes from "./TaskTag.route";
 import authRoutes from "./Auth.route";
+import { AuthMiddleware } from "../middlewares/Auth.middleware";
 
 const router = express.Router();
 
-router.use("/api", taskRoutes);
-router.use("/api", tagRoutes);
-router.use("/api", taskTagRoutes);
 router.use("/api", authRoutes);
+router.use("/api", AuthMiddleware.verifyJwt, taskRoutes);
+router.use("/api", AuthMiddleware.verifyJwt, tagRoutes);
+router.use("/api", AuthMiddleware.verifyJwt, taskTagRoutes);
 
 export default router;
